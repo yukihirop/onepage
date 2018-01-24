9aaee2c6c76350b17fe30b3d73c41443ad4e4ad8
yukihirop <te108186@gmail.com>
2018-01-24 23:35:12 +0900

    Productsコンポーネントを実装した。

---
+++ frontend/src/components/shopping/Products.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  div
+    h1.title All Products
+      p {{ length }} products
+      table.table.is-striped
+        thead
+          tr
+            th Name
+            th Description
+            th Price
+        tbody
+          tr(v-for='product in products' track-by='id')
+            td {{ product.name }}
+            td {{ product.description }}
+            td ${{ product.price }}
+            td
+              button.button.is-info(@click='addToCart(product)') Add to cart
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+const namespace = 'shoppingModule'
+
+export default {
+  name: 'app',
+  computed: mapGetters(namespace, {
+    products: 'allProducts',
+    length: 'getNumberOfProducts'
+  }),
+  methods: mapActions(namespace, [
+    'addToCart'
+  ])
+}
+</script>
