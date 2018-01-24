9350b9762409bf1f55d22ef2734e825330c2ff99
yukihirop <te108186@gmail.com>
2018-01-25 01:01:22 +0900

    ルーティングにカートの画面を追加

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Products from '../components/shopping/Products.vue'
+import Cart from '../components/shopping/Cart.vue'
 
 Vue.use(Router)
 
@@ -9,6 +10,10 @@ export default new Router({
   {
     path: '/',
     component: Products
+  },
+  {
+    path: '/cart',
+    component: Cart
   }
   ]
 })
