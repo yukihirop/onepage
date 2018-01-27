d47e5bbc6007318f6ced30357e334ff2e9e57e5b
yukihirop <te108186@gmail.com>
2018-01-27 19:41:55 +0900

    eslintのparserOptionsの設定
    - コードがECMAScriptのモジュール内にある事を明示
    - グローバルstrictモードを明示
    
    [参考]
    https://eslint.org/docs/user-guide/configuring

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,5 +1,13 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
     extends: "standard",
+    parserOptions: {
+      sourceType: 'module',
+      ecmaFeatures: {
+        impliedStrict: true
+      }
+    },
     plugins: [
       'html'
     ]
