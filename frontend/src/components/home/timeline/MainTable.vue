ca77f14fab20ae0d44e70ad02e58f0603978f15f
yukihirop <te108186@gmail.com>
2018-01-31 22:30:26 +0900

    ホーム画面(タイムライン)のメインテーブルとおすすめユーザーのコンポーネントの実装

---
+++ frontend/src/components/home/timeline/MainTable.vue
@@ -0,0 +1,141 @@
+<template lang="pug">
+.hero
+  article.media
+    .media-left
+      figure.image.is-64x64
+        img(src='https://bulma.io/images/placeholders/128x128.png', alt='Image')
+    .media-content
+      .content
+        p
+          strong John Smith
+          small @johnsmith
+          small 31m
+          br
+          |           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+      nav.level.is-mobile
+        .level-left
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-reply
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-retweet
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-heart
+  article.media
+    .media-left
+      figure.image.is-64x64
+        img(src='https://bulma.io/images/placeholders/128x128.png', alt='Image')
+    .media-content
+      .content
+        p
+          strong John Smith
+          small @johnsmith
+          small 31m
+          br
+          |           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+      nav.level.is-mobile
+        .level-left
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-reply
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-retweet
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-heart
+  article.media
+    .media-left
+      figure.image.is-64x64
+        img(src='https://bulma.io/images/placeholders/128x128.png', alt='Image')
+    .media-content
+      .content
+        p
+          strong John Smith
+          small @johnsmith
+          small 31m
+          br
+          |           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+      nav.level.is-mobile
+        .level-left
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-reply
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-retweet
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-heart
+  article.media
+    .media-left
+      figure.image.is-64x64
+        img(src='https://bulma.io/images/placeholders/128x128.png', alt='Image')
+    .media-content
+      .content
+        p
+          strong John Smith
+          small @johnsmith
+          small 31m
+          br
+          |           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+      nav.level.is-mobile
+        .level-left
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-reply
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-retweet
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-heart
+  article.media
+    .media-left
+      figure.image.is-64x64
+        img(src='https://bulma.io/images/placeholders/128x128.png', alt='Image')
+    .media-content
+      .content
+        p
+          strong John Smith
+          small @johnsmith
+          small 31m
+          br
+          |           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+      nav.level.is-mobile
+        .level-left
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-reply
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-retweet
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-heart
+  article.media
+    .media-left
+      figure.image.is-64x64
+        img(src='https://bulma.io/images/placeholders/128x128.png', alt='Image')
+    .media-content
+      .content
+        p
+          strong John Smith
+          small @johnsmith
+          small 31m
+          br
+          |           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+      nav.level.is-mobile
+        .level-left
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-reply
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-retweet
+          a.level-item
+            span.icon.is-small
+              i.fa.fa-heart
+</template>
---
+++ frontend/src/components/home/timeline/UserList.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.tile.is-ancestor
+  .tile.is-vertical
+    .tile
+      .tile.is-parent.is-vertical
+        article.tile.is-child
+          advertisement
+        article.tile.is-child
+          user-ranking
+</template>
+
+<script>
+import Advertisement from '../Advertisement.vue'
+import UserRanking from '../partial/ranking/User.vue'
+
+export default {
+  components: {
+    Advertisement,
+    UserRanking
+  }
+}
+</script>
