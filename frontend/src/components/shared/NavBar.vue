f9842b829c163c539201c0b2710255cc52deed13
yukihirop <te108186@gmail.com>
2018-01-31 22:06:24 +0900

    Bootstrap-Vueで実装していたNavBarをBulmaで書き直した。
    暫定的に用意したNavBar
    まだまだ作り込んでいく。

--- frontend/src/components/shared/NavBar.vue
+++ frontend/src/components/shared/NavBar.vue
@@ -1,25 +1,48 @@
 <template lang="pug">
-b-navbar(fixed='top', toggleable='md', type='dark', variant='info')
-  b-navbar-toggle(target='nav_collapse')
-  b-navbar-brand(href='#') NavBar
-  b-collapse#nav_collapse(is-nav='')
-    b-navbar-nav
-      b-nav-item(href='#') Link
-      b-nav-item(href='#', disabled='') Disabled
-    // Right aligned nav items
-    b-navbar-nav.ml-auto
-      b-nav-form
-        b-form-input.mr-sm-2(size='sm', type='text', placeholder='Search')
-          b-button.my-2.my-sm-0(size='sm', type='submit') Search
-      b-nav-item-dropdown(text='Lang', right='')
-        b-dropdown-item(href='#') EN
-        b-dropdown-item(href='#') ES
-        b-dropdown-item(href='#') RU
-        b-dropdown-item(href='#') FA
-      b-nav-item-dropdown(right='')
-        // Using button-content slot
-        template(slot='button-content')
-          em User
-        b-dropdown-item(href='#') Profile
-        b-dropdown-item(href='#') Signout
+nav.navbar.is-primary.is-fixed-top
+  .container
+    .navbar-brand
+      a.navbar-item(href='https://bulma.io')
+        img(src='https://bulma.io/images/bulma-logo.png', alt='Bulma: a modern CSS framework based on Flexbox', width='112', height='28')
+      .navbar-burger.burger(data-target='navbarExampleTransparentExample')
+        span
+        span
+        span
+    #navbarExampleTransparentExample.navbar-menu
+      .navbar-start
+        a.navbar-item(href='https://bulma.io/')
+          | Home
+        .navbar-item.has-dropdown.is-hoverable
+          a.navbar-link(href='/documentation/overview/start/')
+            | Docs
+          .navbar-dropdown.is-boxed
+            a.navbar-item(href='/documentation/overview/start/')
+              | Overview
+            a.navbar-item(href='https://bulma.io/documentation/modifiers/syntax/')
+              | Modifiers
+            a.navbar-item(href='https://bulma.io/documentation/columns/basics/')
+              | Columns
+            a.navbar-item(href='https://bulma.io/documentation/layout/container/')
+              | Layout
+            a.navbar-item(href='https://bulma.io/documentation/form/general/')
+              | Form
+            hr.navbar-divider
+            a.navbar-item(href='https://bulma.io/documentation/elements/box/')
+              | Elements
+            a.navbar-item.is-active(href='https://bulma.io/documentation/components/breadcrumb/')
+              | Components
+      .navbar-end
+        .navbar-item
+          .field.is-grouped
+            p.control
+              a.bd-tw-button.button(data-social-network='Twitter', data-social-action='tweet', data-social-target='http://localhost:4000',   target='_blank', href='https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on   Flexbox&hashtags=bulmaio&url=http://localhost:4000&via=jgthms')
+                span.icon
+                  i.fa.fa-twitter
+                span
+                  | Tweet
+            p.control
+              a.button.is-primary(href='https://github.com/jgthms/bulma/archive/0.5.1.zip')
+                span.icon
+                  i.fa.fa-download
+                span Download
 </template>
