94bbe71e8f6a588acd41bdee0b552f92ac8a0230
yukihirop <te108186@gmail.com>
2018-01-31 22:31:53 +0900

    ホーム画面の左側のメニューリストのコンポーネントの実装
    [参考]
    vue-routerの公式ドキュメントを参考にした。
    https://router.vuejs.org/ja/essentials/getting-started.html

---
+++ frontend/src/components/home/MenuList.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+nav.panel
+  router-link.panel-block(to='/trend')
+    span.panel-icon
+      i.fa.fa-bar-chart
+    |     トレンド
+  router-link.panel-block(to='/timeline' exact)
+    span.panel-icon
+      i.fa.fa-list-ul
+    |     タイムライン
+  router-link.panel-block(to='/tagfeed' exact)
+    span.panel-icon
+      i.fa.fa-tags
+    |     タグフィード
+</template>
