3cdde6ab8f919af8f031f1b019445571fab05f69
yukihirop <te108186@gmail.com>
2018-01-31 22:33:14 +0900

    ホーム画面のコンポーネントの実装
    左側メニューリストはVueAffixというパッケージを使用して
    スクロールに対して固定している所がポイント

---
+++ frontend/src/components/home/App.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+.container
+  .columns
+    aside.sidebar.column.is-2
+      //- widthで幅を固定しないとスクロールした時に変わってしまう
+      affix.menu.sidebar-menu(relative-element-selector='#non-affix-content', :offset='{ top: 32, bottom: 0 }', style='width: 160px')
+        menu-list
+    .column
+      #non-affix-content
+        router-view(name='main-table')
+    .column.is-3
+      #non-affix-content
+        router-view(name='ranking')
+</template>
+
+<script>
+import MenuList from './MenuList.vue'
+
+import Vue from 'vue'
+import VueAffix from 'vue-affix'
+
+Vue.use(VueAffix)
+
+export default {
+  components: {
+    MenuList
+  }
+}
+</script>
+
+<!-- https://qiita.com/PoohSunny/items/ff78ed50332ddaf330a8 -->
+<!-- navbar他のコンテンツ重なる -->
+<style scoped lang="stylus">
+.container
+  padding-top: 32px
+</style>
