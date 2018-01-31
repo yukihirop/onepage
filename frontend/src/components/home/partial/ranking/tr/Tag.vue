38fa36bca853d60f36051d19f8c54d08027b3259
yukihirop <te108186@gmail.com>
2018-01-31 22:27:08 +0900

    ユーザー・ランキングやタグ・ランキングのテーブルの行にあたるコンポーネント

---
+++ frontend/src/components/home/partial/ranking/tr/Tag.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+tr(style="padding-bottom: 0px;")
+  td
+    p(style="margin-top: 10px;") {{ rank }}
+  td(style="width: 64px;")
+    img(v-bind:src='imageSrc', style="width: 32px;margin-top: 10px;")
+  td
+    b
+      | {{ tagname }}
+  td
+    b {{ posts }}
+    br
+    small posts
+</template>
+
+<script>
+export default {
+  props: {
+    rank: {
+      type: Number,
+      required: true
+    },
+    imageSrc: {
+      type: String,
+      required: true
+    },
+    tagname: {
+      type: String,
+      required: true
+    },
+    posts: {
+      type: Number,
+      required: true
+    }
+  },
+  // https://kuroeveryday.blogspot.jp/2017/10/img-src-data-binding-vuejs.html
+  data() {
+    return {
+      src: this.imageSrc
+    }
+  }
+}
+</script>
---
+++ frontend/src/components/home/partial/ranking/tr/User.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+tr(style="padding-bottom: 0px;")
+  td
+    p(style="margin-top: 10px;") {{ rank }}
+  td(style="width: 64px;")
+    img(v-bind:src='imageSrc', style="width: 32px;margin-top: 10px;")
+  td
+    p
+      | {{ username }}
+      br
+      small
+        a @{{ mentionname }}
+  td
+    span.panel-icon
+      i.fa.fa-thumbs-o-up(style="margin-top: 10px;")
+        br
+        b {{ likes }}
+</template>
+
+<script>
+export default {
+  props: {
+    rank: {
+      type: Number,
+      required: true
+    },
+    imageSrc: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    mentionname: {
+      type: String,
+      required: true
+    },
+    likes: {
+      type: Number,
+      required: true
+    }
+  },
+  // https://kuroeveryday.blogspot.jp/2017/10/img-src-data-binding-vuejs.html
+  data() {
+    return {
+      src: this.imageSrc
+    }
+  }
+}
+</script>
