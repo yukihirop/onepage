52f0d28d61c0a19a64a5116dd24219176117b49d
yukihirop <te108186@gmail.com>
2018-01-31 22:35:00 +0900

    各画面のルーティングを外部ファイルから読み込むようにした。

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,19 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Products from '../components/shopping/Products.vue'
-import Cart from '../components/shopping/Cart.vue'
+import homeRoutes from './home/index.js'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      component: Products
-    },
-    {
-      path: '/cart',
-      component: Cart
-    }
-  ]
+var router = new Router({
+  linkActiveClass: 'is-active',
+  linkExactActiveClass: 'is-active'
 })
+
+router.addRoutes(homeRoutes)
+
+export default router
