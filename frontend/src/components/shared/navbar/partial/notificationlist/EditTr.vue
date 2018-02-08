ed80dbdd266ebf9dc8ccd8b9e9ce7b902a8bae48
yukihirop <te108186@gmail.com>
2018-02-08 21:14:51 +0900

    NotificationListで使う子コンポーネントの作成

---
+++ frontend/src/components/shared/navbar/partial/notificationlist/EditTr.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+.panel-block
+  article.media
+    figure.media-left
+      p.image.is-32x32
+        img(:src='profileImageSrc', alt="Profile Image")
+    .media-content
+      .content
+        p
+          strong {{ who }}
+          |  が
+          strong 「{{ postTitle }}」
+          |  を 
+          strong 編集
+          |  しました。
+      nav.level.is-mobile
+        .level-left
+          | {{ when }}
+</template>
+
+<script>
+export default {
+  props: {
+    profileImageSrc: {
+      type: String,
+      required: true
+    },
+    who: {
+      type: String,
+      required: true
+    },
+    postTitle: {
+      type: String,
+      required: true
+    },
+    when: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      src: require(this.profileImageSrc)
+    }
+  }
+}
+</script>
