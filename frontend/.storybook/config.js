e057187de85920657d5e18d0cba1701aa594b243
yukihirop <te108186@gmail.com>
2018-02-10 23:17:29 +0900

    一次情報の場所をコメントとして追加

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@storybook/vue'
 
 const loadStories = () => {
   // 参考
+  // https://storybook.js.org/basics/writing-stories/#loading-stories-dynamically
   // https://github.com/carbon-design-system/carbon-components-react/blob/master/.storybook/config.js
   const req = require.context('../src/stories', true, /\.js$/)
   req.keys().forEach(filename => req(filename))
