376ae0d87e5b30094ece317d426814ae9014228c
yukihirop <te108186@gmail.com>
2018-02-17 03:16:25 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212144011) do
+ActiveRecord::Schema.define(version: 20180216181527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "post_likes", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "from_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_likes_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id"
@@ -43,6 +51,7 @@ ActiveRecord::Schema.define(version: 20180212144011) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "post_likes", "posts"
   add_foreign_key "posts", "users"
   add_foreign_key "revisions", "posts"
 end
