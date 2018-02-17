71934543e3894a2bb70236c75938ecb1efdb7864
yukihirop <te108186@gmail.com>
2018-02-17 18:19:24 +0900

    rack-corsの設定を追加
    swagger-uiからAPIを叩くために必要だった
    
    [参考]
    https://github.com/ruby-grape/grape-swagger#cors

---
+++ config/support/rack_cors.rb
@@ -0,0 +1,10 @@
+module Onepage
+  class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+  end
+end
