ecf5840e466a95e086ba3c73a4c716b411826918
yukihirop <te108186@gmail.com>
2018-02-17 22:16:12 +0900

    swagger-uiからput・patch・deleteも出来るようにした

--- config/support/rack_cors.rb
+++ config/support/rack_cors.rb
@@ -3,7 +3,7 @@ module Onepage
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
       end
     end
   end
