976e82c23299d97bac640cdd72e0fa07c299bd32
yukihirop <te108186@gmail.com>
2018-02-22 01:16:40 +0900

    bundle exec rubocop

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  name         :string
+#  kana         :string
+#  organization :string
+#  introduction :text
+#  image        :binary
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Profile < ApplicationRecord
   belongs_to :user
 end
--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  name         :string
+#  kana         :string
+#  organization :string
+#  introduction :text
+#  image        :binary
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 FactoryBot.define do
   factory :profile do
     name         { Faker::Name.name }
