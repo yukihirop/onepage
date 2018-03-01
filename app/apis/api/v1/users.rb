4a3acf35db6e0171cb4ed928dfb330042c8de0f0
yukihirop <te108186@gmail.com>
2018-03-02 02:29:54 +0900

    idで指定したユーザーを更新＆削除できなくした。
    カレントユーザーの更新＆削除はsettings/accountでできるようにしたので
    とりあえず問題ないはず

--- app/apis/api/v1/users.rb
+++ app/apis/api/v1/users.rb
@@ -6,10 +6,6 @@ module API
           ActionController::Parameters.new(params).permit(:email)
         end
 
-        def set_user
-          @user = ::User.find(params[:id])
-        end
-
         def profiles_each_related_user
           # joins.includesでprofilesの要素が取得できないrailsのバグの
           # ためにselect文を使っている
@@ -22,14 +18,6 @@ module API
         desc 'ユーザー一覧を取得します'
         get do
           profiles_each_related_user
-        end
-
-        desc 'idで指定されたユーザーを取得します'
-        params do
-          requires :id, type: Integer
-        end
-        get ':id' do
-          ::User.find(params[:id]).profile
         end
 
         desc 'ユーザーを作成します', {
@@ -45,38 +33,6 @@ module API
           user = ::User.new(user_params)
           if user.save
             user
-          else
-            status 422
-          end
-        end
-
-        desc 'idで指定されたユーザーの更新をします', {
-          failure: [{ code: 422, message: 'Unprocessable Entity' }]
-        }
-        params do
-          requires :id, type: Integer
-          optional :email, type: String, documentation: { param_type: 'body' }
-        end
-        patch':id' do
-          set_user
-          if @user.update(user_params)
-            @user
-          else
-            status 422
-          end
-        end
-
-        desc 'idで指定されたユーザーを削除します', {
-          failure: [{ code: 422, message: 'Unprocessable Entity' }]
-        }
-        params do
-          requires :id, type: Integer
-        end
-        delete ':id' do
-          set_user
-          profile_at_delete_uesr = @user.profile.dup
-          if @user.destroy
-            @user.profile
           else
             status 422
           end
