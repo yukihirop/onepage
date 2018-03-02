9cd9ff47785ae9234f2b0034410d0a76029bfb0e
yukihirop <te108186@gmail.com>
2018-03-03 00:17:26 +0900

    jestでテストする時にassetsの依存関係を良しなにする
    この設定が無いとこのようなエラーが起きる
    ```
    console.error node_modules/vue/dist/vue.runtime.common.js:1715
        /Users/fukudayukihiro/RubymineProjects/onepage/frontend/src/assets/home/user/aimerald.png:1
        ({"Object.<anonymous>":function(module,exports,require,__dirname,__filename,global,jest){�PNG
                                                                                                 ^
    
        SyntaxError: Invalid or unexpected token
            at ScriptTransformer._transformAndBuildScript (/Users/fukudayukihiro/RubymineProjects/onepage/frontend/node_modules/jest-runtime/build/script_transformer.js:307:17)
            at ScriptTransformer.transform (/Users/fukudayukihiro/RubymineProjects/onepage/frontend/node_modules/jest-runtime/build/script_transformer.js:341:19)
            at Runtime._execModule (/Users/fukudayukihiro/RubymineProjects/onepage/frontend/node_modules/jest-runtime/build/index.js:525:53)
            at Runtime.requireModule (/Users/fukudayukihiro/RubymineProjects/onepage/frontend/node_modules/jest-runtime/build/index.js:329:14)
            at Runtime.requireModuleOrMock (/Users/fukudayukihiro/RubymineProjects/onepage/frontend/node_modules/jest-runtime/build/index.js:409:19)
            at Object.data (/Users/fukudayukihiro/RubymineProjects/onepage/frontend/src/components/__test__/factories/users.js:5:7)
            at VueComponent.data (/Users/fukudayukihiro/RubymineProjects/onepage/frontend/src/components/__test__/organisms/mdedia_object/UserMedia.spec.js:15:37)
            at VueComponent.mergedInstanceDataFn (/Users/fukudayukihiro/RubymineProjects/onepage/frontend/node_modules/vue/dist/vue.runtime.common.js:1159:20)
            at getData (/Users/fukudayukihiro/RubymineProjects/onepage/frontend/node_modules/vue/dist/vue.runtime.common.js:3384:17)
            at initData (/Users/fukudayukihiro/RubymineProjects/onepage/frontend/node_modules/vue/dist/vue.runtime.common.js:3343:7)
    ```
    
    参考
    https://github.com/facebook/jest/issues/2663#issuecomment-317109798

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -7,7 +7,9 @@ module.exports = {
   ],
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor'
+    '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor',
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js",
+    "\\.(css|less)$": "<rootDir>/assetsTransformer.js"
   },
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
