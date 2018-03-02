d42c25b54a012fd99cf8b3956ae22db1ff114a20
yukihirop <te108186@gmail.com>
2018-03-03 01:05:53 +0900

    organisms(UserMedia)のスナップショットテストを作成した。
    下記エラーのために
    UserMediaをshallowコピーする際にpropsのprofileImageSrcの
    requiredをfalseに上書きしている。
    
    このprofileImageSrcは子コンポーネントとの共有のプロパティ？
    で子コンポーネントの数だけ設定しないといけない。
    
    だが、子コンポーネントはshallowコピーの場合、<!---->のような
    値にstubされるので設定できないのでrequired:falseのように
    上書きをしている
    (※もしかしたら解釈が間違っているかも)
    
    ```
    console.error node_modules/vue/dist/vue.runtime.common.js:576
        [Vue warn]: Missing required prop: "profileImageSrc"
    ```
    
    dataを上書きする場合は、wrapper.setDataを使うようにする
    さもないと以下のようなエラーがでる
    
    ```
    [Vue warn]: Do not use built-in or reserved HTML elements as component id: data
    ```
    
    [参考]
    https://stackoverflow.com/questions/48953700/how-do-you-set-the-data-values-in-a-component-with-vue-test-utils-before-mounted/49072846#49072846
    
    https://vue-test-utils.vuejs.org/ja/api/options.html#stubs

---
+++ frontend/src/components/__test__/organisms/mdedia_object/UserMedia.spec.js
@@ -0,0 +1,40 @@
+import Vuex from 'vuex'
+import { shallow, createLocalVue } from 'vue-test-utils'
+import UserMedia from '@/components/organisms/media_object/UserMedia.vue'
+import ChildUserMedia from '/components/atoms/media_object/UserMedia.vue'
+import factoryUsers from '@/components/__test__/factories/users'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('UserMedia.vue', () => {
+  it('レンダリングのテスト', () => {
+    // shallowは子コンポーネントまで入り込まない。
+    // <!---->のように置き換えてしまう
+    // そのため？子との共有プロパティーである?
+    // profileImageSrcに値が渡らない
+    // profileImageSrcはrequiredなので実行時にエラーがでるので
+    // falseにして上書きしている
+    // 下記エラーの回避策
+    // console.error node_modules/vue/dist/vue.runtime.common.js:576
+    // [Vue warn]: Missing required prop: "profileImageSrc"
+    const wrapper = shallow(UserMedia, {
+      localVue,
+      props: {
+        profileImageSrc: {
+          type: String,
+          required: false
+        }
+      },
+      // 効果無いような気がすうる
+      stubs: {
+        'child-user-media': ChildUserMedia
+      }
+    })
+    wrapper.setData(factoryUsers.data())
+
+    const template = wrapper.html()
+    expect(template).toMatchSnapshot()
+  })
+})
