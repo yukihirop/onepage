d75053798fcfb6c6b6dbf59535c777c254268fba
yukihirop <te108186@gmail.com>
2018-03-03 21:17:28 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303060944) do
+ActiveRecord::Schema.define(version: 20180303121325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20180303060944) do
     t.bigint "user_id"
     t.index ["post_id"], name: "index_post_likes_on_post_id"
     t.index ["user_id"], name: "index_post_likes_on_user_id"
+  end
+
+  create_table "post_taggings", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_taggings_on_post_id"
+    t.index ["tag_id"], name: "index_post_taggings_on_tag_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -76,6 +85,8 @@ ActiveRecord::Schema.define(version: 20180303060944) do
 
   add_foreign_key "post_likes", "posts"
   add_foreign_key "post_likes", "users"
+  add_foreign_key "post_taggings", "posts"
+  add_foreign_key "post_taggings", "tags"
   add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "revisions", "posts"
