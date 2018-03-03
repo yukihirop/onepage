4bf8e772e3ef7c7c9cbf446a8c210c072c7137cf
yukihirop <te108186@gmail.com>
2018-03-03 15:12:32 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226145852) do
+ActiveRecord::Schema.define(version: 20180303060944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,12 @@ ActiveRecord::Schema.define(version: 20180226145852) do
     t.datetime "updated_at", null: false
     t.text "comment"
     t.index ["post_id"], name: "index_revisions_on_post_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
