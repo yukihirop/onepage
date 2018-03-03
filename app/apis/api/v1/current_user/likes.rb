d3d0f4b9af77accb173445183c286f3903a3e15c
yukihirop <te108186@gmail.com>
2018-03-03 14:48:12 +0900

    CurrentUserでネストするようにした。

--- app/apis/api/v1/current_user/likes.rb
+++ app/apis/api/v1/current_user/likes.rb
@@ -1,19 +1,21 @@
 module API
   module V1
-    class Likes < Grape::API
-      # 参考
-      # https://stackoverflow.com/questions/35344117/why-does-session-doesnt-work-in-grape-rails
-      use ActionDispatch::Session::CookieStore, key: 'user_id'
+    module CurrentUser
+      class Likes < Grape::API
+        # 参考
+        # https://stackoverflow.com/questions/35344117/why-does-session-doesnt-work-in-grape-rails
+        use ActionDispatch::Session::CookieStore, key: 'user_id'
 
-      helpers API::V1::Helpers::SessionHelper
+        helpers API::V1::Helpers::SessionHelper
 
-      resource :likes do
-        desc 'カレントユーザーの「いいね！」一覧を取得します'
-        get do
-          # TODO: 現在は投稿に関する「いいね！」だけを考えているが、
-          #       チャットに対しての「いいね！」も考えるようになったら、
-          #       current_user.chat_likesも足すようにする。
-          current_user.post_likes
+        resource :likes do
+          desc 'カレントユーザーの「いいね！」一覧を取得します'
+          get do
+            # TODO: 現在は投稿に関する「いいね！」だけを考えているが、
+            #       チャットに対しての「いいね！」も考えるようになったら、
+            #       current_user.chat_likesも足すようにする。
+            current_user.post_likes
+          end
         end
       end
     end
