8bd52547d43202a921159080b3bf6ebe890965a9
yukihirop <te108186@gmail.com>
2018-03-04 12:23:10 +0900

    bundle exec annotate

--- app/models/post/tagging.rb
+++ app/models/post/tagging.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: post_taggings
+#
+#  id         :integer          not null, primary key
+#  post_id    :integer
+#  tag_id     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post::Tagging < ApplicationRecord
   belongs_to :post
   belongs_to :tag
