68f75a8bbe9f4f3add45572af67444eb88f2bada
yukihirop <te108186@gmail.com>
2018-03-06 01:19:13 +0900

    active_model_serializerを使うようにしたので使わなくなった。
    これは黒歴史なコミット
    activerecordやactivemodelで解決出来る問題だった。

--- config/support/aggregators_and_sqls.rb
+++
@@ -1,8 +0,0 @@
-module Onepage
-  class Application < Rails::Application
-    config.paths.add File.join('app', 'aggregators'), glob: File.join('**', '*.rb')
-    config.autoload_paths += Dir[Rails.root.join('app', 'aggregators', '*')]
-    config.paths.add File.join('app', 'sqls'), glob: File.join('**', '*.erb')
-    config.autoload_paths += Dir[Rails.root.join('app', 'sqls', '*')]
-  end
-end
