530763d822b0ab31d02a8149b5c15359756f7546
yukihirop <te108186@gmail.com>
2018-03-17 13:38:33 +0900

    API(user)モデルのファクトリーを修正した。
    バックエンド側のAPIの形式が変わると毎回修正が発生する部分なので
    方針として
    
    ・swagger_docで出力されるAPIをコピペする
    ・fakerは使わないようにする
    
    とする。

--- frontend/src/api/__test__/factories/users.js
+++ frontend/src/api/__test__/factories/users.js
@@ -1,17 +1,76 @@
-var faker = require('faker')
-faker.locale = 'ja'
-
 export default {
   user1: {
-    id: 1,
-    email: faker.internet.email(),
-    created_at: faker.date.recent(),
-    updated_at: faker.date.recent()
+    "id": 1,
+    "email": "rosario.funk@marvinpurdy.co",
+    "mention_name": "0gm98",
+    "created_at": "2018-03-03T12:28:30.098Z",
+    "updated_at": "2018-03-03T12:28:30.098Z",
+    "post_likings_count": 2,
+    "profile": {
+      "id": 1,
+      "user_id": 1,
+      "name": "Kristopher Conroy",
+      "kana": "Harley Dach II",
+      "organization": "Watsica-Lind",
+      "introduction": "jsvvy7gvo71jxdqiqp40pk713wmyor33w7zz67hf0qgggtms6jptdt031l5ss3l1gl1v7z6gshvj72cg",
+      "image": "#<File:0x007fe851de23d0>",
+      "created_at": "2018-03-03T12:28:31.480Z",
+      "updated_at": "2018-03-03T12:28:31.480Z"
+    },
+    "post_likings": [
+      {
+        "id": 1,
+        "user_id": 1,
+        "post_id": 1,
+        "from_user_id": 22,
+        "created_at": "2018-03-03T12:28:30.723Z",
+        "updated_at": "2018-03-03T12:28:30.723Z"
+      },
+      {
+        "id": 2,
+        "user_id": 1,
+        "post_id": 1,
+        "from_user_id": 97,
+        "created_at": "2018-03-03T12:28:30.726Z",
+        "updated_at": "2018-03-03T12:28:30.726Z"
+      }
+    ]
   },
   user2: {
-    id: 2,
-    email: faker.internet.email(),
-    created_at: faker.date.recent(),
-    updated_at: faker.date.recent()
-  }
+    "id": 2,
+    "email": "yasmine.schulist@jenkintanton.io",
+    "mention_name": "qhu70",
+    "created_at": "2018-03-03T12:28:30.103Z",
+    "updated_at": "2018-03-03T12:28:30.103Z",
+    "post_likings_count": 2,
+    "profile": {
+      "id": 2,
+      "user_id": 2,
+      "name": "Jailyn Rosenbaum",
+      "kana": "Lonie Kulas",
+      "organization": "Kunde, Labadie and Metz",
+      "introduction": "y5gmu381pthfrs5zvi7way5zd3yj52y5pir22crkx1eogmt93n87n4v3ogy3qw9n7dae5pz1s7744pfc",
+      "image": "#<File:0x007fe84bf5b118>",
+      "created_at": "2018-03-03T12:28:31.489Z",
+      "updated_at": "2018-03-03T12:28:31.489Z"
+    },
+    "post_likings": [
+      {
+        "id": 31,
+        "user_id": 2,
+        "post_id": 4,
+        "from_user_id": 64,
+        "created_at": "2018-03-03T12:28:30.816Z",
+        "updated_at": "2018-03-03T12:28:30.816Z"
+      },
+      {
+        "id": 32,
+        "user_id": 2,
+        "post_id": 4,
+        "from_user_id": 99,
+        "created_at": "2018-03-03T12:28:30.819Z",
+        "updated_at": "2018-03-03T12:28:30.819Z"
+      }
+    ]
+  },
 }
