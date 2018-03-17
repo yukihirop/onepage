35ebe0d92514437f09636aa54d1e00983ca8bb11
yukihirop <te108186@gmail.com>
2018-03-17 17:49:56 +0900

    API(posts)の作成をした。
    カレントユーザーに関係するAPIではなく、
    DBに保存されている投稿一覧を返すAPI

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -37,5 +37,20 @@ FactoryBot.define do
         create_list(:post_tagging, evaluator.post_taggings_count, post: post)
       end
     end
+
+    factory :post_with_revision_and_post_likings_and_user do
+      association :user, factory: :user
+
+      transient do
+        revisions_count 5
+        post_likings_count 5
+      end
+
+      after(:create) do |post, evaluator|
+        create_list(:revision, evaluator.revisions_count, post: post)
+        post.update(newest_revision_id: post.revisions.last.id)
+        create_list(:post_liking, evaluator.post_likings_count, post: post)
+      end
+    end
   end
 end
