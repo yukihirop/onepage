8ffddaa37ffa188a95ddf2e41578dbdf09841084
yukihirop <te108186@gmail.com>
2018-03-18 12:34:45 +0900

    current_userディレクトリ以下のsessionヘルパーを参照するようにした。

--- app/apis/api/v1/current_user/likes.rb
+++ app/apis/api/v1/current_user/likes.rb
@@ -6,7 +6,7 @@ module API
         # https://stackoverflow.com/questions/35344117/why-does-session-doesnt-work-in-grape-rails
         use ActionDispatch::Session::CookieStore, key: 'user_id'
 
-        helpers API::V1::Helpers::SessionHelper
+        helpers API::V1::CurrentUser::Helpers::SessionHelper
 
         namespace 'current_user' do
           resource :likes do
