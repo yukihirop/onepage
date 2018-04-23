0d711cb9f1cdabcc29a797ad9a2f4291fa4ad082
yukihirop <te108186@gmail.com>
2018-04-23 22:20:30 +0900

    CurrentUser::All::Userモデルに、tag_followingsのリレーションを追加
    - テストが漏れていたので追加
    - post_taggingsのリレーションのクラス名がおかしくなっていたので名前を指定した。

--- app/models/api/v1/current_user/all/user.rb
+++ app/models/api/v1/current_user/all/user.rb
@@ -1,6 +1,8 @@
 class API::V1::CurrentUser::All::User < User
   has_many :posts, dependent: :destroy
+  has_many :post_taggings, class_name: 'API::V1::CurrentUser::All::PostTagging', through: :posts
   # API::V1::CurrentUser::Settingスコープのカレントユーザーで使用されるassociation
   has_one :profile, dependent: :destroy
   has_many :post_likings, dependent: :destroy
+  has_many :tag_followings, dependent: :destroy
 end
--- spec/models/api/v1/current_user/all/user_spec.rb
+++ spec/models/api/v1/current_user/all/user_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe API::V1::CurrentUser::All::User, type: :model do
   describe 'association' do
     it { is_expected.to have_many(:posts).dependent(:destroy) }
+    it { is_expected.to have_many(:post_taggings).class_name('API::V1::CurrentUser::All::PostTagging').through(:posts) }
     it { is_expected.to have_one(:profile).dependent(:destroy) }
+    it { is_expected.to have_many(:post_likings).dependent(:destroy) }
+    it { is_expected.to have_many(:tag_followings).dependent(:destroy) }
   end
 end
