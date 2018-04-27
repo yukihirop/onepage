45ca89393af0e762b7cea56d670588eef6f27099
yukihirop <te108186@gmail.com>
2018-04-27 23:50:20 +0900

    Implement API::V1::All::Post.where_filtered_by_tags

--- app/models/api/v1/all/post.rb
+++ app/models/api/v1/all/post.rb
@@ -6,4 +6,7 @@ class API::V1::All::Post < Post
   accepts_nested_attributes_for :revisions
 
   has_many :post_likings, class_name: 'PostLiking', dependent: :destroy
+  has_many :post_taggings, class_name: 'PostTagging', dependent: :destroy
+
+  scope :where_filtered_by_tags, ->(tag_ids) { where(id: API::V1::All::PostTagging.filtered_post_ids_by(tag_ids)) }
 end
