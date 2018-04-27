f828975b1ab413ef3722ecc5b86737aeb02fca73
yukihirop <te108186@gmail.com>
2018-04-27 23:52:20 +0900

    current_userがフォローしているタグがついた投稿一覧を習得するようにした。

--- app/apis/api/v1/all/posts.rb
+++ app/apis/api/v1/all/posts.rb
@@ -6,12 +6,17 @@ module API
         include Grape::ActiveModelSerializers
         include Grape::Kaminari
 
+        # 参考
+        # https://stackoverflow.com/questions/35344117/why-does-session-doesnt-work-in-grape-rails
+        use ActionDispatch::Session::CookieStore, key: 'user_id'
+        helpers API::V1::CurrentUser::Helpers::SessionHelper
+
         helpers do
           # userとrevisionとpost_likingsを持つ
           def serialized_posts
             resource = ActiveModelSerializers::SerializableResource.new(
               paginated_posts,
-              each_serializer: API::V1::PostSerializer,
+              each_serializer: serializer,
               adapter: :json_api,
               serialization_context: ActiveModelSerializers::SerializationContext.new(request)
             )
@@ -19,13 +24,40 @@ module API
           end
 
           def paginated_posts
-            paginate(Post.all)
+            paginate(filtered_posts)
+          end
+
+          def filtered_posts
+            if post_params[:current_user_following_tags]
+              current_user_tag_ids = current_user.tag_followings.pluck(:tag_id)
+              API::V1::All::Post.where_filtered_by_tags(current_user_tag_ids)
+            else
+              API::V1::All::Post.all
+            end
+          end
+
+          def serializer
+            if post_params[:current_user_following_tags]
+              CurrentUserFollowingTagsPostSerializer
+            else
+              PostSerializer
+            end
+          end
+
+          def post_params
+            ActionController::Parameters.new(params).permit(
+              :page,
+              :per_page,
+              :offset,
+              :current_user_following_tags
+            )
           end
 
           params :query_params do
             optional :page,     type: Integer, documentation: { params_type: 'query' }
             optional :per_page, type: Integer, documentation: { params_type: 'query' }
             optional :offset,   type: Integer, documentation: { params_type: 'query' }
+            optional :current_user_following_tags, type: Boolean, documentation: { params_type: 'query' }
           end
         end
 
--- spec/factories/api/v1/all/post_taggings.rb
+++ spec/factories/api/v1/all/post_taggings.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :api_v1_post_tagging, class: API::V1::All::PostTagging do
+    sequence(:post_id)
+    sequence(:tag_id)
+
+    trait :with_tag do
+      tag_id { create(:api_v1_tag).id }
+    end
   end
 end
