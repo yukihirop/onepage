d8aa46631e3a71a4edf049e647b84a421968a129
yukihirop <te108186@gmail.com>
2018-04-28 01:30:13 +0900

    Implement API::V1::All::PostLikingSerializer

---
+++ spec/serializers/api/v1/all/post_liking_serializer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe API::V1::All::PostLikingSerializer do
+  describe 'attirbutes' do
+    it { is_expected.to have_attribute(:id) }
+    it { is_expected.to have_attribute(:user_id) }
+    it { is_expected.to have_attribute(:post_id) }
+    it { is_expected.to have_attribute(:from_user_id) }
+    it { is_expected.to have_attribute(:created_at) }
+    it { is_expected.to have_attribute(:updated_at) }
+  end
+end
