0091f53ac7736d88ff50dec8eae5b034800e0990
yukihirop <te108186@gmail.com>
2018-04-27 23:39:40 +0900

    Implement API::V1::All::PostTagging.filtered_post_ids_by

--- app/models/api/v1/all/post_tagging.rb
+++ app/models/api/v1/all/post_tagging.rb
@@ -1,3 +1,10 @@
 class API::V1::All::PostTagging < PostTagging
   belongs_to :tag, optional: true
+  belongs_to :post, optional: true
+
+  class << self
+    def filtered_post_ids_by(tag_ids = [])
+      where(tag_id: tag_ids).pluck(:post_id).uniq
+    end
+  end
 end
