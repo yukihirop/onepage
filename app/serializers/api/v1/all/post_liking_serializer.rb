71ef8baf87e9c52bf9a7e5f7009be61c02166d86
yukihirop <te108186@gmail.com>
2018-04-28 22:00:02 +0900

    API::V1::All::PostLikingSerializerを実装した。

---
+++ app/serializers/api/v1/all/post_liking_serializer.rb
@@ -0,0 +1,9 @@
+module API
+  module V1
+    module All
+      class PostLikingSerializer < ::PostLikingSerializer
+        attributes :id, :user_id, :post_id, :from_user_id, :created_at, :updated_at
+      end
+    end
+  end
+end
