7db5d4cf7dc7f6d8ea0e9147cfbee62f61259698
yukihirop <te108186@gmail.com>
2018-04-29 14:14:24 +0900

    Paginationコンポーネントを作成した。

---
+++ frontend/src/components/atoms/pagination/Pagination.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+nav.pagination.is-centered(role='navigation' arial-label='pagination')
+  paginate(
+    :page-count="pageCount"
+    :margin-pages="0"
+    :page-range="5"
+    :initial-page="0"
+    :click-handler="clickHander"
+    :container-class="'pagination-list'"
+    :page-link-class="'pagination-link'"
+    :prev-link-class="'pagination-previous'"
+    :next-link-class="'pagination-next'"
+    :active-class="'is-current'"
+    :first-last-button="true"
+  )
+</template>
+
+<script>
+  export default {
+    props:{
+      pageCount:{
+        type: Number,
+        required: true
+      },
+      clickHander:{
+        type: Function,
+        required: true
+      }
+    }
+  }
+</script>
