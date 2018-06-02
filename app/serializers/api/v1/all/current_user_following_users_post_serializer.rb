b917c9124c38e60393d3914f35136af7990da652
yukihirop <te108186@gmail.com>
2018-06-03 01:51:10 +0900

    CurrentUserFollowingUsersPostSerializerを実装した。
    下記のrubocopの警告をさけるために、Metrics/BlockLengthのルールを無効にした。
    ```
    spec/serializers/api/v1/all/current_user_following_users_post_serializer_spec.rb:3:1: C: Metrics/BlockLength: Block has too many lines. [86/77]
    RSpec.describe API::V1::All::CurrentUserFollowingUsersPostSerializer, type: :serializer do ...
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    2 files inspected, 1 offense detected
    Rubocopに失敗しました
    Press Enter to continue
    ```

--- app/serializers/api/v1/all/current_user_following_users_post_serializer.rb
+++ app/serializers/api/v1/all/current_user_following_users_post_serializer.rb
@@ -2,26 +2,48 @@ module API
   module V1
     module All
       class CurrentUserFollowingUsersPostSerializer < ::PostSerializer
-        attributes :user, :profile, :revision, :post_likings_count, :comments_count
+        attributes :follower_profile, :revision, :post_liking, :comment, :resource_count, :action_type
 
-        def user
-          object.user
-        end
-
-        def profile
-          object.user.profile
+        def follower_profile
+          case action_type
+          when 'post'
+            object.user.profile
+          when 'post_liking'
+            User.find(post_liking.user_id).profile
+          when 'comment'
+            User.find(comment.user_id).profile
+          else
+            object.user.profile
+          end
         end
 
         def revision
           object.revisions.find(object.newest_revision_id)
         end
 
-        def post_likings_count
-          object.post_likings.count
+        def post_liking
+          object.post_likings.find_by(post_id: object.id)
         end
 
-        def comments_count
-          object.comments.count
+        def comment
+          object.comments.find_by(post_id: object.id)
+        end
+
+        def resource_count
+          case action_type
+          when 'post'
+            nil
+          when 'post_liking'
+            nil
+          when 'comment'
+            object.comments.count
+          else
+            nil
+          end
+        end
+
+        def action_type
+          object.action_type || ''
         end
       end
     end
--- spec/serializers/api/v1/all/current_user_following_users_post_serializer_spec.rb
+++ spec/serializers/api/v1/all/current_user_following_users_post_serializer_spec.rb
@@ -2,29 +2,51 @@ require 'rails_helper'
 
 RSpec.describe API::V1::All::CurrentUserFollowingUsersPostSerializer, type: :serializer do
   describe 'attirbutes' do
-    it { is_expected.to have_attribute(:user) }
-    it { is_expected.to have_attribute(:profile) }
+    it { is_expected.to have_attribute(:follower_profile) }
     it { is_expected.to have_attribute(:revision) }
-    it { is_expected.to have_attribute(:post_likings_count) }
-    it { is_expected.to have_attribute(:comments_count) }
+    it { is_expected.to have_attribute(:post_liking) }
+    it { is_expected.to have_attribute(:comment) }
+    it { is_expected.to have_attribute(:resource_count) }
+    it { is_expected.to have_attribute(:action_type) }
   end
 
   describe 'instance methods' do
     let(:post) { create(:api_v1_post_with_revision_and_post_likings_and_user) }
     let(:serialized_post) { described_class.new(post) }
 
-    describe '#user' do
-      let(:result) { post.user }
-      subject { serialized_post.user }
+    describe '#follower_profile' do
+      let(:action_type) { nil }
+      before { allow(serialized_post).to receive(:action_type).and_return(action_type) }
 
-      it { is_expected.to eq result }
-    end
+      subject { serialized_post.follower_profile }
 
-    describe '#profile' do
-      let(:result) { post.user.profile }
-      subject { serialized_post.profile }
+      context 'action_typeが「post」の時' do
+        let(:result) { post.user.profile }
+        let(:action_type) { 'post' }
 
-      it { is_expected.to eq result }
+        it { is_expected.to eq result }
+      end
+
+      context 'action_typeが「post_liking」の時' do
+        let(:result) { API::V1::All::User.find(serialized_post.post_liking.user_id).profile }
+        let(:action_type) { 'post_liking' }
+
+        it { is_expected.to eq result }
+      end
+
+      context 'action_typeが「comment」の時' do
+        let(:result) { API::V1::All::User.find(serialized_post.comment.user_id).profile }
+        let(:action_type) { 'comment' }
+
+        it { is_expected.to eq result }
+      end
+
+      context 'action_typeがその他の時' do
+        let(:result) { post.user.profile }
+        let(:aciton_type) { '' }
+
+        it { is_expected.to eq result }
+      end
     end
 
     describe '#revision' do
@@ -34,18 +56,68 @@ RSpec.describe API::V1::All::CurrentUserFollowingUsersPostSerializer, type: :ser
       it { is_expected.to eq result }
     end
 
-    describe '#post_likings_count' do
-      let(:result) { post.post_likings.count }
-      subject { serialized_post.post_likings_count }
+    describe '#post_liking' do
+      let(:result) { post.post_likings.find_by(post_id: post.id) }
+      subject { serialized_post.post_liking }
 
       it { is_expected.to eq result }
     end
 
-    describe '#comments_count' do
-      let(:result) { post.comments.count }
-      subject { serialized_post.comments_count }
+    describe '#comment' do
+      let(:result) { post.comments.find_by(post_id: post.id) }
+      subject { serialized_post.comment }
 
       it { is_expected.to eq result }
+    end
+
+    describe '#resource_count' do
+      let(:action_type) { nil }
+      before { allow(serialized_post).to receive(:action_type).and_return(action_type) }
+
+      subject { serialized_post.resource_count }
+
+      context 'action_typeが「post」の時' do
+        let(:action_type) { 'post' }
+
+        it { is_expected.to eq nil }
+      end
+
+      context 'action_typeが「post_liking」の時' do
+        let(:action_type) { 'post_liking' }
+
+        it { is_expected.to eq nil }
+      end
+
+      context 'action_typeが「comment」の時」' do
+        let(:action_type) { 'comment' }
+        let(:result) { post.comments.count }
+
+        it { is_expected.to eq result }
+      end
+
+      context 'action_typeがその他の時' do
+        let(:action_type) { '' }
+
+        it { is_expected.to eq nil }
+      end
+    end
+
+    describe '#action_type' do
+      subject { serialized_post.action_type }
+
+      context 'action_typeが存在する時' do
+        let(:result) { post.action_type }
+
+        before { post.action_type = 'comment' }
+
+        it { is_expected.to eq result }
+      end
+
+      context 'action_typeが存在しない時' do
+        let(:result) { '' }
+
+        it { is_expected.to eq result }
+      end
     end
   end
 end
