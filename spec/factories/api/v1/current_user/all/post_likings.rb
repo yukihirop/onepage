3726f342dc374c84bf7feaad900ac96b81fe6842
yukihirop <te108186@gmail.com>
2018-06-10 15:03:34 +0900

    post_likingsのファクトリーデータを修正した。

--- spec/factories/api/v1/current_user/all/post_likings.rb
+++ spec/factories/api/v1/current_user/all/post_likings.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :api_v1_current_user_post_liking, class: API::V1::CurrentUser::All::PostLiking do
-    from_user_id { Faker::Number.number(2).to_i }
+    to_user_id { Faker::Number.number(2).to_i }
   end
 end
